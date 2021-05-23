b299b4bf683e126fde659d3dec52a308c6f34c27
Dejan <33637358+Dejan85@users.noreply.github.com>
2021-05-23 11:37:14 +0200

    update

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,13 +1,20 @@
 import { getAllEvents } from "../../dummy-data";
 import EventList from "../../components/events/EventsList";
 import EventsSearch from "../../components/events/EventsSearch";
+import { useRouter } from "next/router";
 
 const EventsPage = () => {
   const events = getAllEvents();
+  const router = useRouter();
+
+  const findEventsHandler = (year, month) => {
+    const fullPath = `/events/${year}/${month}`;
+    router.push(fullPath);
+  };
 
   return (
     <>
-      <EventsSearch />
+      <EventsSearch onSearch={findEventsHandler} />
       <EventList items={events} />
     </>
   );
